fea3a03ae62d8835a9d4b38630b74fceeab502b3
Mouli Somesetty <msomeset@HYD-SomesetM-M.local>
2020-10-04 20:08:48 +0530

    Resolve changes

--- src/main/java/com/example/restservice/GreetingController.java
+++ src/main/java/com/example/restservice/GreetingController.java
@@ -9,11 +9,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
-
+	@Autowired
+	private Greeting greeting;
+	
+	@CrossOrigin(origins = "https://moulikiran.github.io/demoprofile/")
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		//greeting.setContent(name);
+		return greeting;
+	}
+	
+	@CrossOrigin(origins = "https://moulikiran.github.io/demoprofile/")
+	@GetMapping("/greeting/selectcomponent")
+	public void dateFromMobile(@RequestParam(value = "name", defaultValue = "World") String name) {
+		greeting.setContent(name);
 	}
 }
